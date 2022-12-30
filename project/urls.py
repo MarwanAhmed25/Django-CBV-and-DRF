4aab1c963cfd361ba66b1f0a7f45803c4ea7eb13
MarwanAhmed25 <marwan4125881@gmail.com>
2022-12-30 21:54:02 +0200

    add comments for some urls

--- project/urls.py
+++ project/urls.py
@@ -5,15 +5,15 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('products/', include('products.urls', namespace='products')),
-    path('types/', include('product_types.urls', namespace='types')),
-    path('models/', include('product_models.urls', namespace='models')),
-    path('api/v1/products/', include('products.api.urls')),
-    path('api/v1/types/', include('product_types.api.urls')),
-    path('api/v1/models/', include('product_models.api.urls')),
-    path('api-auth', include('rest_framework.urls')),
-    path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('products/', include('products.urls', namespace='products')), # cbv 
+    path('types/', include('product_types.urls', namespace='types')), #cbv
+    path('models/', include('product_models.urls', namespace='models')), #cbv
+    path('api/v1/products/', include('products.api.urls')), # rest_framework.generic
+    path('api/v1/types/', include('product_types.api.urls')), # rest_framework.generic
+    path('api/v1/models/', include('product_models.api.urls')), # rest_framework.generic
+    path('api-auth/', include('rest_framework.urls')),  # rest_framework [ login & logout ]
+    path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')), # all auth api except registration
+    path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')), # api registration
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
